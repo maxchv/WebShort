b0b8a09ee0f9c88bc79ab29f93c5a47d61482740
Maxim <maxshaptala@gmail.com>
2020-07-03 21:39:15 +0300

    Добавлена форма

--- module03/lesson01/demo/server.js
+++ module03/lesson01/demo/server.js
@@ -4,14 +4,25 @@
 
 const http = require('http');
 
+const form = `<form method='post'>
+    login: <input name='login' require/>
+    <br/>
+    passw: <input type='password' name='password' require />
+    <br/>
+    <input type='submit'/>
+</form>`;
+
 const server = http.createServer(function (req, resp) {
     // запрос
     console.log("Method:", req.method);
     console.log("Url: ", req.url);
     console.log("Headers: ", req.headers);
-
-    // формируем ответ
-    resp.end('Hello Web from Server');
+    if (req.method == 'POST') {
+        resp.end("<h1>Welcome to secret page!!!</h1>")
+    } else {
+        // формируем ответ
+        resp.end(form);
+    }
 });
 
 server.listen(3000, 'localhost', function () {
