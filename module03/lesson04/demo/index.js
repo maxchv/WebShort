d6a17a58cb558c4367c36983d38f1fc4bc8735d6
Maxim Shaptala <maxshaptala@gmail.com>
2020-07-13 21:00:59 +0300

    Добавлен пример обработки post запроса

--- module03/lesson04/demo/index.js
+++ module03/lesson04/demo/index.js
@@ -1,12 +1,45 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 
+app.use(function (request, response, next) {
+    console.log("Middleware for " + request.url);
+    next();
+});
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({
+    extended: false
+}));
+
 app.get("/", function (request, response) {
-    response.send("<h1>Привет Web using ExpressJs</h1>")
+    response.send("<h1>Привет Web using ExpressJs</h1>" +
+        "<ul><li><a href='/todo'>Todo</a></li></ul>")
 });
 
 app.get("/about", function (request, response) {
     response.send("<h1>About this site</h1>")
+});
+
+app.get("/todo", function (request, response) {
+    response.sendFile(__dirname + "/public/todo.html");
+});
+
+app.post("/todo", function (request, response) {
+    console.log(request.body);
+    const task = request.body.task;
+    fs.appendFile(__dirname + "/db/todo.txt", task + "\n", function () {
+
+    });
+
+    fs.readFile(__dirname + "/public/todo.html", {
+        encoding: "utf-8"
+    }, function (err, html) {
+        html = html.replace("{{todo}}", task);
+        response.send(html);
+    });
+    //response.sendFile(__dirname + "/public/todo.html");
 });
 
 const students = [{
