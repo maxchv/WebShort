e3abbd03d72830ca1699539e53cd2a6b6326bacc
Maxim Shaptala <maxshaptala@gmail.com>
2020-07-15 22:21:11 +0300

    удалены посты

--- module03/lesson05/blog/app.js
+++ module03/lesson05/blog/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const postRouter = require("./routes/post");
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/post", postRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
